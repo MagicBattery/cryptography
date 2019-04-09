ff8f2417263d3a7617cff758ef667d51147aa50a
Lachlan Cooke <c3329722@uon.edu.au>
2019-04-09 13:15:12 +0000

    Tidied everything up. Added substitution cipher for encryption only.

--- main.c
+++ main.c
@@ -1,84 +1,172 @@
 //Rotation cipher encryption
 
+/*               MODE TABLE
+   _________________________________________
+  |     |                                   |
+  |  0  |  Rotation - Encrypt               | 
+  |_____|___________________________________|
+  |     |                                   |
+  |  1  |  Rotation - Decrypt (Key)         | 
+  |_____|___________________________________|
+  |     |                                   |
+  |  2  |  Rotation - Decrypt (No Key)      |
+  |_____|___________________________________|
+  |     |                                   |
+  |  3  |  Substitution - Encrypt           |
+  |_____|___________________________________|
+  |     |                                   |
+  |  4  |  Substitution - Decrypt (Key)     |
+  |_____|___________________________________|
+  |     |                                   |
+  |  5  |  Substitution - Decrypt (No Key)  |
+  |_____|___________________________________|
+  
+  
+  */
+
 #include <stdio.h>
 
-void rotationEnc(char *msg, char *cipher, int k, int size); //Rotation cipher function prototype.
-void rotationDec(char *msg, char *cipher, int k, int size);
-void substitution(char *msg, char *cipher, int k, int size); //Substitution cipher function prototype.
+void rotationEnc(char *msg, int k, int size); //Rotation cipher function prototype (encryption mode)
+void rotationDec(char *msg, int k, int size); //Rotation cipher function prototype (decryption mode)
+void rotationDecNoKey(char *msg, int k, int size); //Rotation cipher function prototype (decryption mode)
+
+void substitutionEnc(char *msg, char *s, int size); //Substitution cipher function prototype
+void substitutionDec(char *msg, char *s, int size); //Substitution cipher function prototype
+void substitutionDecNoKey(char *msg, char *s, int size); //Substitution cipher function prototype
 
 int main() {
     
-  int dir = 0;
-    
+  int mode = 0; //0 for encryption, 1 for decryption
   printf("Welcome to cool cats cipher!\n");
-  printf("Enter 0 for encryption, or 1 for decryption: ");
-  scanf("%d", &dir);
-
-  char msg[] = "MJQQT"; //Hard-coded input (for now)
-  int k = 5; //Rotation cipher key
+  printf("Choose a mode and press enter: ");
+  scanf("%d", &mode);
+  
+  /////////////////////////////////////////////
+  
+  char msg[] = "AAA"; //Hard-coded input (for now)
   int size = (int)sizeof(msg);
   
-  char cipher[size]; //Declare an output array to store the encrypted message (size is optimised)
+  /////////////////////////////////////////////
   
-  if(dir == 0) {
-      printf("Encrypting...");
-      rotationEnc(msg, cipher, k, size);
-  } else if(dir == 1) {
-      printf("Decrypting...");
-      rotationDec(msg, cipher, k, size);
+  int k = 5; //Rotation cipher key
+  char s[26] = "QAZXSWEDCVFRTGBNHYUJMKILOP"; //Substitution cipher key
+  
+  /////////////////////////////////////////////
+  
+  switch(mode) { //User's input for mode determines which function is called
+      case 0:
+              rotationEnc(msg, k, size);
+              break;
+              
+      case 1:
+              rotationDec(msg, k, size);
+              break;
+              
+      case 2:
+              rotationDecNoKey(msg, k, size);
+              break;
+              
+      case 3:
+              substitutionEnc(msg, s, size);
+              break;
+              
+      case 4:
+              substitutionDec(msg, s, size);
+              break;
+      
+      case 5:
+              substitutionEncNoKey(msg, s, size);
+              break;
+              
+      default: printf("Please choose a valid mode (0-5)");
+               break;
   }
- 
-   //Call the rotate cipher function to perform the encryption
 
   printf("File output!\n");
 
   return 0;
 }
 
-void rotationEnc(char *msg, char *cipher, int k, int size) { //Rotation cipher which takes the input message array, the empty output array, rotation key and size (of the array) as arguments
+
+
+
+
+
+///////////////////// ROTATION CIPHER ENCRYPTION ////////////////////////
+void rotationEnc(char *msg, int k, int size) { //Rotation cipher which takes the input message array, the empty output array, rotation key and size (of the array) as arguments
     
-  FILE * encrypted; //Delare a file pointer
-  
+  FILE * encrypted; //Delare a pointer to a file
+  char temp;
   encrypted = fopen("output.txt", "w");  //Open a new file
     
-  for(int i = 0; i < (size - 1); i++) { //Iterate through each elemen in the msg array, converting each character to be encrypted, then storing in the cipher array.
+  for(int i = 0; i < (size - 1); i++) { //Iterate through each element in the msg array, converting each character to be encrypted, then storing in the cipher array.
       if(msg[i] == ' ') {
-          cipher[i] = msg[i];
-          fprintf(encrypted, "%c", cipher[i]); //Print to file
+          fprintf(encrypted, " "); //Print to file
       } else {
-          cipher[i] = ((msg[i] - 65 + k)%26) + 65;
-          fprintf(encrypted, "%c", cipher[i]); //Print to file
+          temp = ((msg[i] - 65 + k)%26) + 65;
+          fprintf(encrypted, "%c", temp); //Print to file
       }
-  }
+    } 
+
   
   fclose(encrypted); //Close the file
   
   return;
 }
 
-void rotationDec(char *msg, char *cipher, int k, int size) { //Rotation cipher which takes the input message array, the empty output array, rotation key and size (of the array) as arguments
+
+////////////// ROTATION CIPHER DECRYPTION (KEY) ////////////////////
+void rotationDec(char *msg, int k, int size) {
     
-  FILE * encrypted; //Delare a file pointer
-  
-  encrypted = fopen("output.txt", "w");  //Open a new file
+    FILE * encrypted; //Delare a pointer to a file
+    char temp;
+    encrypted = fopen("output.txt", "w");  //Open a new file
     
-  for(int i = 0; i < (size - 1); i++) { //Iterate through each elemen in the msg array, converting each character to be encrypted, then storing in the cipher array.
+    for(int i = 0; i < (size - 1); i++) { //Iterate through each element in the msg array, converting each character to be encrypted, then storing in the cipher array.
       if(msg[i] == ' ') {
-          cipher[i] = msg[i];
-          fprintf(encrypted, "%c", cipher[i]); //Print to file
+          fprintf(encrypted, " "); //Print to file
       } else {
-          cipher[i] = ((msg[i] - 65 - k)%26) + 65;
-          fprintf(encrypted, "%c", cipher[i]); //Print to file
+          temp = ((msg[i] - 65 - k)%26) + 65;
+          fprintf(encrypted, "%c", temp); //Print to file
       }
-  }
+    }
+    
+    fclose(encrypted); //Close the file
   
-  fclose(encrypted); //Close the file
-  
-  return;
+    return;
 }
 
-void substitution(char *msg, char *cipher, int k, int size) {
+////////////// ROTATION CIPHER DECRYPTION (NO KEY) ////////////////////
+void rotationDecNoKey(char *msg, int k, int size) {
     
 }
+
+////////////// SUBSTITUTION CIPHER ENCRYPTION ////////////////////
+void substitutionEnc(char *msg, char *s, int size) {
+    
+    FILE * encrypted; //Delare a pointer to a file
+    char temp;
+    encrypted = fopen("output.txt", "w");  //Open a new file
+    
+    for(int i = 0; i < (size - 1); i++) {
+        
+        if(msg[i] == ' ') {
+          fprintf(encrypted, " "); //Print to file  
+        } else {
+            temp = s[msg[i] - 65]; //Index of the letter being read (1-26)
+            fprintf(encrypted, "%c", temp);
+        }
+        
+    }
+    
+}
+
+////////////// SUBSTITUTION CIPHER DECRYPTION (KEY) ////////////////////
+void substitutionDec(char *msg, char *s, int size) {
+    
+}
+
+////////////// SUBSTITUTION CIPHER DECRYPTION (NO KEY) ////////////////////
+void substitutionDecNoKey(char *msg, char *s, int size);
 
 
